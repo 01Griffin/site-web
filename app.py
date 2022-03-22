f0e3a584dd6fc2ecc2609212f5ba777729e012c4
Griffin <alfhiem.system@gmail.com>
2022-03-22 11:59:35 -0300

    primary commint

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+from resources.tasks import tasks_bp
+
+
+
+app = Flask(__name__)
+
+
+app.register_blueprint(tasks_bp)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
